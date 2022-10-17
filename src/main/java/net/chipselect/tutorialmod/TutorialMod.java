be5d64707f146f0e2afeca27b27363ddd55c21b8
ChipNSelect <reedrifter@gmail.com>
2022-10-17 18:58:11 +0800

    Initial commit

---
+++ src/main/java/net/chipselect/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.chipselect.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+	public static final String MOD_ID = "Tutorial Mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
